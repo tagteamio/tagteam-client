aec0c969fd51feb0c916a5e7d2ad8564111a3259
Chiel Kunkels <chiel@kunkels.me>
2014-02-07 01:23:24 +0100

    Added some basic styling
    - Normalize for consistency
    - Bourbon for scss mixins
    - Basic layout for scss source files
    - Updated grunt config to watch scss files
    - Include compiled styles in layouts

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,55 @@
 
 module.exports = function(grunt){
 	grunt.initConfig({
+		concurrent: {
+			dev: {
+				tasks: ['nodemon', 'watch'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
+
+		mkdir: {
+			all: {
+				options: {
+					create: ['public/css']
+				}
+			}
+		},
+
 		nodemon: {
 			dev: {}
+		},
+
+		sass: {
+			dev: {
+				files: {
+					'public/css/base.css': 'src/css/base.scss'
+				}
+			}
+		},
+
+		watch: {
+			css: {
+				files: [
+					'src/css/*.scss', 'src/css/**/*.scss'
+				],
+				tasks: ['sass:dev']
+			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-mkdir');
 	grunt.loadNpmTasks('grunt-nodemon');
-	grunt.registerTask('default', ['nodemon']);
+	grunt.loadNpmTasks('grunt-sass');
+
+	grunt.registerTask('default', [
+		'mkdir',
+		'sass:dev',
+		'concurrent:dev'
+	]);
 };
 
